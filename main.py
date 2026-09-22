47477f3cab8d6e4065fb03cc66ac09023a9fed6d
Julia Gorb <ulia.gor.2010@yandex.ru>
2026-09-22 23:06:50 +0300

    lesson 1

---
+++ main.py
@@ -0,0 +1,11 @@
+main = "Julia-Julia"
+age = 27
+height = 1.72
+is_student = True
+nothing = None
+
+print(main, age, height, is_student, nothing)
+print(type(main), type(age), type(height), type(is_student), type(nothing))
+
+year = int("2026")
+print(year + 1)
